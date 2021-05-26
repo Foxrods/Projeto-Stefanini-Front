bef5fb2650a9717452b89f17de3418580aca17fb
Foxrods <rodolforangel@get.inatel.br>
2021-05-26 11:04:35 -0300

    front com ambiente docker ou local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,14 @@
 const host = '127.0.0.1';
+//Local
+// export const environment = {
+//   production: false,
+//   api_url: `http://${host}:5000/api`
+// };
+
+//Docker Environment: porta 5010
 export const environment = {
-  production: false,
-  api_url: `http://${host}:5000/api`
+	production: false,
+	api_url: `http://${host}:5010/api`
 };
 
 
